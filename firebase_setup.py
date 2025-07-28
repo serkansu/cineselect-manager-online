f51cdd0f033816b011b3cbd74492e7a99a5e20f8
serkansu <suluns.tr@gmail.com>
2025-07-28 18:07:26 +0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -5,9 +5,13 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 def get_database():
-    # Base64 olarak kaydedilmiş JSON stringini al
+    # Ortam değişkeninden base64 stringi al
     encoded_key = os.environ.get("FIREBASE_SERVICE_KEY_B64")
     decoded_json = base64.b64decode(encoded_key).decode("utf-8")
+
+    # Parse etmeden önce PEM satır sonlarını düzelt
+    decoded_json = decoded_json.replace('\\n', '\n')
+
     service_account_info = json.loads(decoded_json)
 
     # Firebase'e bağlan
