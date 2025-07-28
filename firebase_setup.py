528af3a50c4b78fecd98d91d7cde1b7c680da5ca
serkansu <suluns.tr@gmail.com>
2025-07-28 18:05:29 +0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,17 +1,19 @@
 import os
 import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, db
 
 def get_database():
-    # Ortam değişkeninden JSON stringini al ve parse et
-    firebase_key_json = os.environ.get("FIREBASE_SERVICE_KEY")
-    service_account_info = json.loads(firebase_key_json)
+    # Base64 olarak kaydedilmiş JSON stringini al
+    encoded_key = os.environ.get("FIREBASE_SERVICE_KEY_B64")
+    decoded_json = base64.b64decode(encoded_key).decode("utf-8")
+    service_account_info = json.loads(decoded_json)
 
     # Firebase'e bağlan
     cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://cine-select-default-rtdb.europe-west1.firebasedatabase.app/'
+        'databaseURL': 'https://cine-select-default-rtdb.europe-west1.firebasedatabase.app'
     })
 
     return db
