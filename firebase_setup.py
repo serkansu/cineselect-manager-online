e5bab7cea050e3ceb8e7abbe76174d598e4f568e
serkansu <suluns.tr@gmail.com>
2025-07-28 18:13:55 +0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -5,19 +5,17 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 def get_database():
-    # Ortam değişkeninden base64 stringi al
+    # Base64 ortam değişkenini al ve decode et
     encoded_key = os.environ.get("FIREBASE_SERVICE_KEY_B64")
     decoded_json = base64.b64decode(encoded_key).decode("utf-8")
-
-    # Parse etmeden önce PEM satır sonlarını düzelt
     decoded_json = decoded_json.replace('\\n', '\n')
-
     service_account_info = json.loads(decoded_json)
 
-    # Firebase'e bağlan
-    cred = credentials.Certificate(service_account_info)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://cine-select-default-rtdb.europe-west1.firebasedatabase.app'
-    })
+    # Sadece ilk kez çalıştırıldığında initialize et
+    if not firebase_admin._apps:
+        cred = credentials.Certificate(service_account_info)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://cine-select-default-rtdb.europe-west1.firebasedatabase.app'
+        })
 
     return db
