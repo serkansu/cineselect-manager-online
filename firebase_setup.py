b60106ea036539eab5b609149307daae6836c262
serkansu <suluns.tr@gmail.com>
2025-07-28 18:22:45 +0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -6,14 +6,11 @@ from firebase_admin import credentials, db
 
 def get_database():
     encoded_key = os.environ.get("FIREBASE_SERVICE_KEY_B64")
+
+    # Doğrudan base64 decode et ve JSON olarak yükle
     decoded_json = base64.b64decode(encoded_key).decode("utf-8")
+    service_account_info = json.loads(decoded_json)
 
-    # JSON içindeki kaçış karakterlerini düzelt
-    corrected = decoded_json.encode('utf-8').decode('unicode_escape')
-
-    service_account_info = json.loads(corrected)
-
-    # initialize_app sadece bir kez çağrılsın
     if not firebase_admin._apps:
         cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred, {
